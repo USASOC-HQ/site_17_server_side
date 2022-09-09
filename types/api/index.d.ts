e42c91dc0d80f1ee5ed0bc168652fc7f1d0084e9
erwinel <leonard.t.erwine.ctr@mail.mil>
2022-09-09 13:32:08 -0400

    Moved files from SetupAndConfig repository e99e2bb33a333e1cab8e58a5990931bc77f3c917/source/x_g_inte_site_17/server

---
+++ types/api/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="AtfHelper.d.ts" />
+/// <reference path="DistinguishedNameContext.d.ts" />
+/// <reference path="LocationApproval.d.ts" />
+/// <reference path="ProfileValidator.d.ts" />
+/// <reference path="ReservationScheduler.d.ts" />
+/// <reference path="ReservationSchedulerAjax.d.ts" />
+/// <reference path="Site17Util.d.ts" />
---
+++ types/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="sn_typings_server_scoped/dist/index.d.ts" />
+/// <reference path="table/index.d.ts" />
+/// <reference path="api/index.d.ts" />
